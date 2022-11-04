a0af91ba10e4a1b4574d45af3c9066a267d90f90
Asus <chyngyzturusbekov1@gmail.com>
2022-11-04 19:34:53 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat cat=new Cat("Muska",3,"     Ovcharka");
+        System.out.println(cat.getName()+" "+cat.getAge()+"  year old"+cat.getPoroda()+" ");
+
+
+        Dog dog=new Dog("Mukhtar",5,"    Mushyk");
+
+        System.out.println(dog.getName()+" "+dog.getAge()+" years"+dog.getPoroda()+" ");
+
+
+//        Phone phone=new Phone("Iphone","13 pro max","Black");
+//        System.out.println(phone.getName()+" "+phone.getModel()+" "+phone.getColor()+" ");
+//
+//        Phone phone1 = new Phone("Samsung"," Z flip","Red");
+//        System.out.println(phone1.getName()+" "+phone1.getModel()+" "+phone1.getColor
+    }
+}
